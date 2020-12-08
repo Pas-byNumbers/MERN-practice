ed4b831cb77f6d4098334c8814ea7a044eb0fdd0
Pas-byNumbers <pasbynumbers@gmail.com>
2020-12-08 12:45:00 +0000

    modularised server functions

--- environment1/serve-html/main.js
+++ environment1/serve-html/main.js
@@ -3,61 +3,39 @@
 const http = require("http");
 const httpStatus = require("http-status-codes");
 const fs = require("fs");
-
+const router = require("./router");
 const portNumber = 3000;
-
-const sendErrorResponse = (res) => {
-  res.writeHead(httpStatus.NOT_FOUND, {
+const plainTextContentType = {
+    "Content-Type": "text/plain",
+  },
+  htmlContentType = {
     "Content-Type": "text/html",
-  });
-  res.write("<h1>404 - Page Not Found!</h1>");
-  res.end();
-};
-
-const customReadFile = (file_path, res) => {
-    if (fs.existsSync(file_path)) {
-      fs.readFile(file_path, (error, data) => {
-        if (error) {
-          console.log(error);
-          sendErrorResponse(res);
-          return;
-        }
-        res.write(data);
-        res.end();
-  });
-  } else {
-      sendErrorResponse(res);
-    }
   };
+
+const customReadFile = (file, res) => {
+  fs.readFile(`./${file}`, (errors, data) => {
+    if (errors) {
+      console.log("Error reading the file...");
+    }
+    res.end(data);
+  });
+};
 
 const app = http.createServer();
 
-app.on("request", (req, res) => {
-  let urlReq = req.url;
-  if (urlReq.indexOf(".html") !== -1) {
-    res.writeHead(httpStatus.OK, {
-      "Content-Type": "text/html",
-    });
-    customReadFile(`./views${urlReq}`, res);
-  } else if (urlReq.indexOf(".js") !== -1) {
-    res.writeHead(httpStatus.OK, {
-      "Content-Type": "text/javascript",
-    });
-    customReadFile(`./public/js${urlReq}`, res);
-  } else if (urlReq.indexOf(".css") !== -1) {
-    res.writeHead(httpStatus.OK, {
-      "Content-Type": "text/css",
-    });
-    customReadFile(`./public/css${urlReq}`, res);
-  } else if (urlReq.indexOf(".png") !== -1) {
-    res.writeHead(httpStatus.OK, {
-      "Content-Type": "image/png",
-    });
-    customReadFile(`./public/images${urlReq}`, res);
-  } else {
-    sendErrorResponse(res);
-  }
+// you can write routes here or in router.js
+router.get("/", (req, res) => {
+  res.writeHead(httpStatus.OK, plainTextContentType);
+  res.end("INDEX");
+});
+router.get("/home", (req, res) => {
+  res.writeHead(httpStatus.OK, htmlContentType);
+  customReadFile("views/home.html", res);
+});
+router.post("/", (req, res) => {
+  res.writeHead(httpStatus.OK, plainTextContentType);
+  res.end("POSTED");
 });
 
-app.listen(portNumber);
+app.on("request", router.handle).listen(portNumber);
 console.log(`The server has started and is listening on PORT:${portNumber}`);
