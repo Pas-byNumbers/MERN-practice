786d125ff923e3e24fad37695f69e907407d02da
Pas-byNumbers <pasbynumbers@gmail.com>
2021-02-07 21:40:33 +0000

    added error handling and public folder

--- environment2/express_templates/main.js
+++ environment2/express_templates/main.js
@@ -5,11 +5,20 @@ const portNumber = process.env.PORT || 3000,
 
 const expressEjsLayouts = require("express-ejs-layouts");
 const homeController = require('./controllers/homeController');
+const errorController = require('./controllers/errorController');
+
+
 app.listen(portNumber, () => {
     console.log(`Express.js server listening on PORT:${portNumber}`);
 });
 app.set("view engine", "ejs");
 app.use(expressEjsLayouts);
+app.use(express.static("public"))
+// app.use(errorController.logErrors)
+
 
 
 app.get("/name/:myName", homeController.renderName)
+
+app.use(errorController.respondNoResourceFound);
+app.use(errorController.respondInternalError);
